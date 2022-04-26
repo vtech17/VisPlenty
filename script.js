19a354c5f4e4643d3e4d045faace2363d4aae6ff
Gaurika M <gaurikam2003@gmail.com>
2022-04-26 13:47:53 -0400

    added in color blind button

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // Get the modal
 var modal = document.getElementById("modal-1");
+var modal2 = document.getElementById("modal-2");
 
 // Get the button that opens the modal
 var btn = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
 
 var slider = document.getElementById("myRanger");
 
@@ -53,15 +56,22 @@ document.addEventListener('DOMContentLoaded', function() {
 span.onclick = function() {
   modal.style.display = "none";
 }
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target == (modal || modal2)) {
     modal.style.display = "none";
+    modal2.style.display = "none";
   }
 }
